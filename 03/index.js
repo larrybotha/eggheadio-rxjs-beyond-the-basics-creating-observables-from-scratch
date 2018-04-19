71cdadacd32032857b3d98c363147ed48cbf478e
Larry Botha <larry@fixate.it>
2018-04-19 10:09:09 +0200

    contrast generators with observables

--- 03/index.js
+++ 03/index.js
@@ -1,15 +1,23 @@
 const Rx = require('rxjs');
 
+const log = (...args) => console.log(args.join());
+
 function foo() {
   console.log('Hello');
 
   return 42;
 }
 
-console.log(foo.call());
+log(foo.call());
+log('\n');
 
+// An Observable is a producer of values
+// The difference with Observables and generators is that the Observable
+// determines when values are sent, whereas with generators the iterator created
+// from a generator determines when a value is sent
+// Observables utilise a PUSH style
 const bar = Rx.Observable.create(observer => {
-  console.log('Hello');
+  log('Hello');
 
   observer.next(42);
   observer.next(100);
@@ -21,4 +29,29 @@ const bar = Rx.Observable.create(observer => {
   }, 500);
 });
 
-bar.subscribe(x => console.log(x));
+// A subscriber is a consumer of values
+bar.subscribe(x => log('from observable', x));
+log('\n');
+
+// Observables are similar to generators - a generator can emit multiple values
+// Like an Observable, a generator won't emit anything until an iterator is
+// created, and next is called on that iterator
+// A generator is a producer of values
+//
+function* myGen() {
+  log('Hello');
+
+  yield 42;
+  yield 100;
+  yield 200;
+  yield 300;
+}
+
+// an iterator is a consumer of values
+// Iterators determine when a value is obtained, in contrast to subscribers.
+// Iterators and generators utilise a PULL strategy to obtain values
+const myIterator = myGen();
+log(myIterator.next().value);
+log(myIterator.next().value);
+log(myIterator.next().value);
+log(myIterator.next().value);
