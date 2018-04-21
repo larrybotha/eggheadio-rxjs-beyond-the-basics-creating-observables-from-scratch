b78ed5a14cdcb2b3414f6b860976efd7d853a89f
Larry Botha <larry@fixate.it>
2018-04-21 09:03:56 +0200

    demonstrate creating and using a basic implementation of fromEventPattern

--- 08/index.js
+++ 08/index.js
@@ -1,19 +1,45 @@
-const Rx = require('rxjs');
-
 const log = (...args) => console.log(args.join());
 
-const arr = [42, 100, 200];
+function addEventHandler(handler) {
+  document.addEventListener('click', handler);
+}
 
-const arrObs = Rx.Observable.from(arr);
+function removeEventHandler(handler) {
+  document.removeEventListener('click', handler);
+}
 
-arrObs.subscribe(
-  function onNext(x) {
-    log(`arrObs`, x);
-  },
-  function onError(e) {
-    log('something wrong inside Observable', e);
-  },
-  function onComplete() {
-    log('done');
-  }
+// const foo = fromEventPattern(addEventHandler, removeEventHandler);
+
+// Rx.Observable.fromEventPattern can be used to subscribe to both DOM event
+// streams and Node event streams
+const foo1 = Rx.Observable.fromEventPattern(
+  addEventHandler,
+  removeEventHandler
+);
+
+// we can manually implement a basic fromEventPattern like so:
+function fromEventPattern(add, remove) {
+  // create an observable
+  return Rx.Observable.create(observer => {
+    // every time we receive an event, push that event onto the stream
+    add(ev => observer.next(ev));
+  });
+}
+
+// There's also fromEvent, which is specifically for creating Observables from
+// DOM events
+const foo2 = Rx.Observable.fromEvent(document, 'click');
+
+[foo1, foo2].map((o, i) =>
+  o.subscribe(
+    function onNext(x) {
+      log(`from ${i}`, x);
+    },
+    function onError(e) {
+      log('something wrong inside Observable', e);
+    },
+    function onComplete() {
+      log('done');
+    }
+  )
 );
