dcc27d18981719f0af33040c4e31d887c79ad395
Larry Botha <larry@fixate.it>
2018-04-22 17:54:50 +0200

    add timer and interval observers

--- 10/index.js
+++ 10/index.js
@@ -2,16 +2,35 @@ const Rx = require('rxjs');
 
 const log = (...args) => console.log([...args]);
 
-const foo = Rx.Observable.empty();
+// Rx.Observable.interval is a convenience operator for the following:
+// const intervalObs = Rx.Observable.create(observer => {
+//   let v = 0;
 
-foo.subscribe(
-  function onNext(x) {
-    log(`obs ${k}`, x);
-  },
-  function onError(e) {
-    log(`something wrong inside ${k}`, e);
-  },
-  function onComplete() {
-    log(`${k} done`);
-  }
+//   setInterval(() => {
+//     observer.next(v);
+//     v = v + 1;
+//   }, 1000);
+// });
+const intervalObs = Rx.Observable.interval(1000);
+
+// timer allows us to start an interval after a specific start time
+const timerObs = Rx.Observable.timer(3000, 1000);
+
+const obsMap = {
+  intervalObs,
+  timerObs,
+};
+
+Object.keys(obsMap).map(k =>
+  obsMap[k].subscribe(
+    function onNext(x) {
+      log(k, x);
+    },
+    function onError(e) {
+      log(`something wrong inside ${k}`, e);
+    },
+    function onComplete() {
+      log(k, 'done');
+    }
+  )
 );
